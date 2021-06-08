d4ea8779e2d700b39c469dd7b5a1723d6ee6c897
leti9302 <alexpskov60@gmail.com>
2021-06-08 20:51:29 +0300

    oh

--- QRCode.cpp
+++ QRCode.cpp
@@ -5,7 +5,7 @@ void main(int argc, char** argv) {
 	try {
 		InitializeMagick(*argv);
 		setlocale(LC_ALL, "Russian");
-		string _string = "Хабррфвпфвпфвпроыафупгценклмр";
+		string _string = "Хабрр";
 		Bits* _bits = new Bits(_string);
 		Image image = Draw::BaseElements(_bits);
 		image.write("ebat.png");
